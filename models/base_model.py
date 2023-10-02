79ada00127f166a312e98e1949de3da01d194ee1
ZAKosma <zakosma@gmail.com>
2023-10-02 15:34:02 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,16 @@ import models
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        if kwargs != {}:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
